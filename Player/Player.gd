09f93bb0df2deb82e2dc7365887b5a544ecebef9
mariothedog <mariothedogmc@gmail.com>
2020-03-21 23:06:29 +0000

    Added basic player input

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,1 +1,27 @@
 extends Area2D
+
+const SPEED = 300
+
+var velocity = Vector2()
+var speed_multiplier = 1
+
+func _physics_process(delta):
+	_get_input()
+	_movement(delta)
+
+func _get_input():
+	var input_vel = Vector2()
+	
+	if Input.is_action_pressed("move_right"):
+		input_vel.x += 1
+	if Input.is_action_pressed("move_left"):
+		input_vel.x -= 1
+	if Input.is_action_pressed("move_up"):
+		input_vel.y -= 1
+	if Input.is_action_pressed("move_down"):
+		input_vel.y += 1
+	
+	velocity = input_vel.normalized() * SPEED * speed_multiplier
+
+func _movement(delta):
+	position += velocity * delta
