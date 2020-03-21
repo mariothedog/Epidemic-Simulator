135fc642f17b0b7e20751dcbb92dded3d36a10e8
mariothedog <mariothedogmc@gmail.com>
2020-03-21 23:34:04 +0000

    Added a health bar to the humans.

--- Human/Human.gd
+++ Human/Human.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 var infected = false
 
+func _ready():
+	$"Health Bar".visible = false
+
 func infect():
 	infected = true
+	$"Health Bar".visible = true
 	$AnimationPlayer.play("Infect")
