94e93fb5afef60e8cb1ed2f7572b6b6dff338316
mariothedog <mariothedogmc@gmail.com>
2020-03-22 11:36:07 +0000

    Improved the HUD.

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 func _process(_delta):
-	$"MarginContainer/HBoxContainer/Spread Chance".text = str(stepify(global.virus_spread_chance, 0.01))
-	
-	$"MarginContainer/HBoxContainer/Level Name".text = "Level %s %s" % [global.current_level, global.epidemic_names[global.current_level-1]]
+	$"MarginContainer/HBoxContainer/Details".text = "Level %s %s\nAlive: %s\nSpread Chance: %s" % [
+		global.current_level,
+		global.epidemic_names[global.current_level-1],
+		get_parent().get_node("Humans").get_child_count(),
+		str(stepify(global.virus_spread_chance, 0.01))
+	]
